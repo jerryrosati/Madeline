5969ee8b4a6803ef9631799b86076cad941416aa
Jerry <jerry@rosati.io>
2020-06-06 02:04:13 -0400

    Added arguments and error handling to the schedule command

--- commands/schedule.js
+++ commands/schedule.js
@@ -5,15 +5,27 @@ module.exports = {
     name: 'schedule',
     description: 'Schedule a thing.',
     args: true,
+    usage: `<date (in format MM/DD/YYYY)> <time (HH:mm)>`,
 
     execute(message, args) {
-        let date = new Date(2020, 5, 6, 0, 22, 0);
-        message.channel.send(`@everyone Scheduling event for ${date.toString()}`);
-        utils.logError(message, "error");
+        if (args.length < 2) {
+            message.reply(`Wrong number of arguments; the proper usage is \`!schedule ${this.usage}\``);
+            return;
+        }
+
+        const dateArray = args[0].split(/[.-\/]/);
+        console.log(dateArray);
+
+        const timeArray = args[1].split(/[:]/);
+        console.log(timeArray);
+
+        // Subtract 1 from the month because months are zero-indexed in JS (0 = January, 11 = December).
+        let date = new Date(dateArray[2], dateArray[0] - 1, dateArray[1], timeArray[0], timeArray[1], 0);
+        message.channel.send(`Scheduling event for ${date.toString()}`);
 
         let job = schedule.scheduleJob(date, () => {
             console.log(date.toString());
-            message.channel.send("This is a reminder");
+            message.channel.send("<@377193147203059713> This is a reminder");
         })
     },
 };
