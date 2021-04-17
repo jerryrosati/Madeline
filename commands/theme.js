127f9d61c4a3d2682d8867217da9caf9e59d6ca8
Jerry <jerry@rosati.io>
2021-04-17 03:18:11 -0400

    Added flag for choosing op or ed

--- commands/theme.js
+++ commands/theme.js
@@ -1,15 +1,42 @@
-const { MessageAttachment } = require('discord.js')
+/**
+ * Searches Anithemes.moe for an anime opening or ending song and sends it to the channel.
+ *
+ * Usage: !theme title [-t OP|ED [-n number]]
+ */
 const fetch = require('node-fetch')
 
 module.exports = {
     name: 'theme',
     description: 'Search for anime theme',
-    usage: 'anime_title',
+    usage: 'anime_title [-t OP|ED [-n number]]',
     args: true,
     argsOptional: false,
 
     execute(message, args) {
+        const typeFlagIndex = args.indexOf('-t')
+        const numberFlagIndex = args.indexOf('-n')
+        let type = 'OP'
+        let number = ''
+        if (typeFlagIndex !== -1) {
+            if (typeFlagIndex < args.length - 1 && (['op', 'ed'].includes(args[typeFlagIndex + 1].toLowerCase()))) {
+                type = args[typeFlagIndex + 1]
+                args.splice(typeFlagIndex, 2)
+            } else {
+                args.splice(typeFlagIndex, 1)
+            }
+        }
+
+        if (numberFlagIndex !== -1) {
+            if (numberFlagIndex < args.length - 1) {
+                number = args[numberFlagIndex + 1]
+                args.splice(numberFlagIndex, 2)
+            } else {
+                args.splice(numberFlagIndex, 1)
+            }
+        }
+
         const seriesTitle = args.join(' ')
+        console.log(`title: ${seriesTitle}`)
         const animeThemesUrl = `https://staging.animethemes.moe/api/search?q=${seriesTitle}&limit=1&fields[search]=anime&fields[anime]=themes`
         const queryOptions = {
             method: 'GET',
@@ -23,8 +50,8 @@ module.exports = {
             .then(response => response.json())
             .then(json => {
                 console.log(json)
-                const themeList = json.search.anime[0].themes
-                let baseName = themeList[0].entries[0].videos[0].basename
+                const themeList = json.search.anime[0].themes.filter(theme => theme.type === type.toUpperCase())
+                const baseName = themeList[0].entries[0].videos[0].basename
                 message.reply(`https://animethemes.moe/video/${baseName}`)
             }).catch(error => {
                 console.error(error)
