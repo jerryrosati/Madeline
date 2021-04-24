4009deda8b994560f2649f1a343bf7bd1b562383
Jerry <jerry@rosati.io>
2021-04-24 18:57:05 -0400

    Updated the animethemes query string for the !theme command

--- commands/theme.js
+++ commands/theme.js
@@ -37,7 +37,7 @@ module.exports = {
         console.log(`type = ${type}, number = ${themeNum}`)
 
         // The query variables.
-        const animeThemesUrl = `https://staging.animethemes.moe/api/search?q=${seriesTitle}&limit=1&fields[search]=anime&fields[anime]=themes`
+        const animeThemesUrl = `https://staging.animethemes.moe/api/search?q=${seriesTitle}&fields[search]=anime&include[anime]=themes.entries.videos&limit=1`
         const queryOptions = {
             method: 'GET',
             headers: {
