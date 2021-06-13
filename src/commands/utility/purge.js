60f12aebc64914532440dd84eda29eeff8210b52
Jerry <jerry@rosati.io>
2021-06-13 02:23:43 -0400

    Updated purge to delete the command messages as well as the purged messages, and to log the purged messages

--- src/commands/utility/purge.js
+++ src/commands/utility/purge.js
@@ -4,7 +4,7 @@
  * Usage: !purge 20
  */
 const { Command } = require('discord.js-commando')
-
+const Discord = require('discord.js')
 module.exports = class PurgeCommand extends Command {
     constructor(client) {
         super(client, {
@@ -41,13 +41,48 @@ module.exports = class PurgeCommand extends Command {
         console.log(`Messages to delete: ${amount}`)
 
         try {
-            const messagesToDelete = await message.channel.messages.fetch({ limit: amount, before: message.id })
+            // Fetch the list of messages to delete. Add 3 to account for the message with the purge command,
+            // Madeline's confirmation message, and then the user's confirmation message
+            const messagesToDelete = await message.channel.messages.fetch({ limit: amount + 3 })
             console.log(`messageToDelete size = ${messagesToDelete.size}`)
+
+            // Delete the messages.
             const deletedMessages = await message.channel.bulkDelete(messagesToDelete)
-            console.log(`Bulk deleted ${deletedMessages.size} messages from channel ${message.channel.name}.`)
+
+            // Log the purge to the console and to the purge-log channel.
+            console.log(`Bulk deleted ${deletedMessages.size} messages (${amount} + ${deletedMessages.size - amount}) from channel ${message.channel.name}.`)
+            this.logPurge(message, messagesToDelete, amount)
         } catch (error) {
             console.error(error)
             message.reply('Failed to delete messages')
         }
+    }
+
+    logPurge(message, messagesToDelete, amount) {
+        const messageText = messagesToDelete
+            .sort((messageA, messageB) => messageA.createdTimestamp - messageB.createdTimestamp)
+            .map(message => `[${message.createdAt.toLocaleTimeString()}] ${message.author.tag}: ${message.content}`)
+            .slice(0, amount)
+            .join('\n')
+
+        const embed = new Discord.MessageEmbed()
+            .setDescription(messageText)
+
+        message.guild.members.fetch(message.author.id)
+            .then(member => {
+                embed.setAuthor(`${amount} messages deleted by ${member.user.tag}`, member.user.avatarURL())
+                embed.setColor(member.displayColor)
+
+                message.client.guilds.cache
+                    .filter(guild => guild.available)
+                    .each(guild => {
+                        guild.channels.cache
+                            .filter(channel => channel.name === 'purge-log')
+                            .each(channel => channel.send(embed))
+                    })
+            })
+            .catch(error => {
+                console.log('Error while fetching user', error)
+            })
     }
 }
