57f7a528dfdb3480c269ff472313a92e040b7340
Jerry Rosati <jerry@rosati.io>
2022-02-21 00:04:50 -0500

    Moved events to their own directory

--- src/index.js
+++ src/index.js
@@ -43,72 +43,20 @@ for (const file of commandFiles) {
 console.log('Found the following files:');
 commandFiles.forEach(name => console.log(name));
 
-// When the client is ready, run this code (only once).
-client.once('ready', () => {
-    console.log('Ready!');
-});
+// Search for all the events.
+const eventFiles = fs.readdirSync('./src/events').filter(file => file.endsWith('.js'));
 
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return;
+for (const file of eventFiles) {
+    // Initialize each of the events.
+    const event = require(`./events/${file}`);
 
-    const command = client.commands.get(interaction.commandName);
-
-    if (!command) return;
-
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    // Handle each event.
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
     }
-});
+}
 
 // Login to Discord with your client's token.
 client.login(token);
-
-/*
-const { prefix, devID, token, defaultBroadcastChannelName } = require('./../config.json')
-const { CommandoClient } = require('discord.js-commando')
-const path = require('path')
-
-// Create a new discord client
-const client = new CommandoClient({
-    commandPrefix: prefix,
-    owner: devID
-})
-
-// Register the commands.
-client.registry
-    .registerDefaultTypes()
-    .registerGroups([
-        ['admin', 'Admin commands'],
-        ['media', 'Commands related to media (anime, manga, etc.)'],
-        ['utility', 'Utility commands'],
-        ['misc', 'Other commands']
-    ])
-    .registerDefaultGroups()
-    .registerDefaultCommands()
-    .registerCommandsIn(path.join(__dirname, 'commands'))
-
-// Triggered when the client is ready, which will happen once after login.
-client.once('ready', () => {
-    console.log('Ready!')
-    client.user.setActivity('the chat zoom by', { type: 'WATCHING' })
-})
-
-// Triggered when a message is sent.
-client.on('message', async message => {
-    // If a message is sent in a broadcast channel, then broadcast is to all other servers with that channel.
-    // Need to manually look for the broadcast command because this won't have the normal command format of !broadcast <message>.
-    if (message.channel.name === defaultBroadcastChannelName && !message.author.bot) {
-        const commands = client.registry.commands
-        const broadcastCommand = commands.get('broadcast')
-        broadcastCommand.run(message, { text: message.content })
-    }
-})
-
-client.on('error', error => console.error(error))
-
-// Login to discord with the token
-client.login(token)
-*/
